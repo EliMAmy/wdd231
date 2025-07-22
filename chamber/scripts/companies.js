c2e3eac4fdcff688d56789ddd42644e43bdd1554
EliMAmy <rociomarapaza@gmail.com>
2025-07-21 23:48:59 -0500

    add

--- chamber/scripts/companies.js
+++ chamber/scripts/companies.js
@@ -1,5 +1,11 @@
-const url= "data/members.json"
+
+const url = "data/members.json"
 const cards = document.querySelector('#cards');
+const table = document.querySelector("#table");
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+
+
 
 async function getCompanyData() {
     const response = await fetch(url); // request
@@ -7,8 +13,15 @@ async function getCompanyData() {
     console.log(data); // temp output test of data response
     displayCompanies(data.companies);
 }
-
 getCompanyData();
+async function getCompanyDataTable() {
+    const response = await fetch(url); // request
+    const data = await response.json(); // parse the JSON data
+    console.log(data); // temp output test of data response
+    displaytheCompanies(data.companies);
+}
+getCompanyDataTable();
+
 
 const displayCompanies = (companies) => {
     companies.forEach((company) => {
@@ -30,7 +43,7 @@ const displayCompanies = (companies) => {
         phone.innerHTML = `<span class="label">PHONE:</span> ${company.phone}`;
         email.innerHTML = `<span class="label">EMAIL:</span> ${company.email}`;
         portrait.setAttribute("src", company.imageurl);
-        portrait.setAttribute("alt", `${company.imageurl}`)
+        portrait.setAttribute("alt", `${company.imageurl}`);
         portrait.setAttribute("loading", "lazy");
         card.setAttribute("class", "card");
         division.setAttribute("class", "onecard");
@@ -49,3 +62,50 @@ const displayCompanies = (companies) => {
         cards.appendChild(card);
     });
 }
+
+
+
+
+
+const displaytheCompanies = (companies) => {
+    companies.forEach((company) => {
+        //  build code goes here
+        let body = document.createElement("tbody");
+        let division = document.createElement("tr")
+        let fullName = document.createElement("th");
+        let portrait = document.createElement("td");
+        let address = document.createElement("td");
+        let phone = document.createElement("td");
+
+
+        fullName.textContent = company.name;
+        address.innerHTML = `<span class="label">Address:</span> ${company.address}`;
+        phone.innerHTML = `<span class="label">Phone:</span> ${company.phone}`;
+        portrait.setAttribute("src", company.imageurl);
+        portrait.setAttribute("alt", `${company.imageurl}`);
+        portrait.setAttribute("loading", "lazy");
+        body.setAttribute("class", "table-design");
+
+        // Append the section(table) with the created elements
+        division.appendChild(fullName);
+        division.appendChild(portrait);
+        division.appendChild(phone);
+        division.appendChild(address);
+        body.appendChild(division);
+        table.appendChild(body);
+    });
+}
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+    // example using arrow function
+    table.classList.add("hide");
+    cards.classList.remove("hide")
+});
+
+listbutton.addEventListener("click", () => {
+    // example using arrow function
+    cards.classList.add("hide");
+    table.classList.remove("hide")
+});
